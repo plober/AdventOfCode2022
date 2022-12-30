dc97c3259578a3dd8fceed53f99c3e11f24744ad
Plober <plober@gmail.com>
2022-12-30 18:17:39 -0300

    Rtas día 03

--- 03-Solution.py
+++ 03-Solution.py
@@ -5,18 +5,58 @@ lines, largo=[], []
 with open("input03.txt") as file_object:
   lines = file_object.readlines()
 
-for line in lines:
+for line in lines: 
   largo.append(line.rstrip())
 
 
-corto=[]
+corto="""vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw""".splitlines()
 
 
+class ItemsMochila:
 
-RespuestaCortaA, RespuestaA, RespuestaCortaB, RespuestaB = 'N/A', 'N/A', 'N/A', 'N/A'
+    def numero_letra(caracter): 
+        return ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.find(caracter)
 
+    def repetidos_mitades(string_items):
+        return set(string_items[:len(string_items)//2]) & set(string_items[len(string_items)//2:])
+    
+    def suma_repetidos(inventarios):
+        total = 0
+        for lista in inventarios:
+            total += ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.find((set(lista[:len(lista)//2]) & set(lista[len(lista)//2:])).pop())
+        return total
+    
+    def repetido_en_tres(triplete):
+        [elfo_1, elfo_2, elfo_3] = triplete
+        return set(elfo_1) & set(elfo_2) & set(elfo_3)
 
-print("	La respuesta del Ejemplo A es " + str(RespuestaCortaA))
+    def suma_tripletes(lista):
+        suma_total = 0
+        for numero in range (0,len(lista),3):
+            repetido=set(lista[numero]) & set(lista[numero+1]) & set(lista[numero+2])
+            suma_total += ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.find(repetido.pop())
+            # print(suma_total)
+        return suma_total
+
+            
+assert ItemsMochila.repetidos_mitades(corto[0]).pop()=='p'
+assert ItemsMochila.repetidos_mitades(corto[1]).pop()=='L'
+assert ItemsMochila.repetidos_mitades(corto[2]).pop()=='P'
+assert ItemsMochila.repetidos_mitades(corto[3]).pop()=='v'
+assert ItemsMochila.repetidos_mitades(corto[4]).pop()=='t'
+assert ItemsMochila.repetidos_mitades(corto[5]).pop()=='s'
+ 
+assert ItemsMochila.suma_repetidos(corto)== 157
+RespuestaA, RespuestaB = ItemsMochila.suma_repetidos(largo), 'N/A'
+
 print("	La respuesta A es " + str(RespuestaA))
-print("	La respuesta del Ejemplo B es " + str(RespuestaCortaB))
-print("	La respuesta B es " + str(RespuestaB))
+
+assert ItemsMochila.repetido_en_tres(corto[:3]).pop()=='r'
+assert ItemsMochila.repetido_en_tres(corto[3:][:3]).pop()=='Z'
+
+print("	La respuesta B es " + str(ItemsMochila.suma_tripletes(largo)))
--- newOne.py
+++ newOne.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.10
 
-numero="02"
+numero="03"
 
 archivoSolucion = numero+"-Solution.py"
 inputlargoFile = "input"+numero+".txt"
@@ -11,11 +11,7 @@ with open(archivoSolucion, 'a') as file_object:
     file_object.write("lines, largo=[], []\n")
     file_object.write("with open(\""+inputlargoFile+"\") as file_object:\n  lines = file_object.readlines()\n\n")
     file_object.write("for line in lines: \n  largo.append(line.rstrip())\n\n\n")
-    file_object.write("corto=[]\n\n")
+    file_object.write("corto=[].split('\n')\n\n")
     file_object.write("RespuestaA, RespuestaB = 'N/A', 'N/A'\n\n\n")
     file_object.write('print("\tLa respuesta A es " + str(RespuestaA))\n')
     file_object.write('print("\tLa respuesta B es " + str(RespuestaB))\n')
-
-# with open(inputcortoFile) as file_object:lines = file_object.readlines()
-# inputcorto=[]
-# for line in lines: inputcorto.append(line.rstrip())
