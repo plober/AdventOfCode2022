d9551c435cc2c764edec3dadc00b6841938e3532
Plober <plober@gmail.com>
2023-01-02 10:12:06 -0300

    Día 04

--- 04-Solution.py
+++ 04-Solution.py
@@ -9,12 +9,51 @@ for line in lines:
   largo.append(line.rstrip())
 
 
-corto=[]
+corto="""2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8""".splitlines()
 
-RespuestaCortaA, RespuestaA, RespuestaCortaB, RespuestaB = 'N/A', 'N/A', 'N/A', 'N/A'
+class CampCleanup:
+    def uno_contenido_en_otro(tramos):
+        # [intervalos for intervalos in [par.split(",") for par in corto]]
+        contenidos_encontrados=0
+        for par in tramos:
+            # par = "5-7,7-9"
+            par.replace(",","-")
+            # par = "5-7-7-9"
+            menor_1, mayor_1, menor_2, mayor_2 = [int(valor) for valor in par.replace(",","-").split("-")]
 
+            if [menor_1, mayor_1] == [menor_2, mayor_2]:
+                contenidos_encontrados += 1
+                continue
+                        
+            if menor_1 <= menor_2 and mayor_2 <= mayor_1:
+                contenidos_encontrados += 1
+                
+            if menor_2 <= menor_1 and mayor_1 <= mayor_2:
+                contenidos_encontrados += 1
+        return contenidos_encontrados
+            
+    def hay_intersecciones(tramos):
+        intersecciones_encontradas=0
+        for par in tramos:
+            par.replace(",","-")
+            menor_1, mayor_1, menor_2, mayor_2 = [int(valor) for valor in par.replace(",","-").split("-")]
+            if len(set(range(menor_1, mayor_1+1)) & set(range(menor_2, mayor_2+1))) > 0: 
+                intersecciones_encontradas += 1
+        return intersecciones_encontradas
+ 
+respuesta_corta_a = CampCleanup.uno_contenido_en_otro(corto)
+respuesta_larga_a = CampCleanup.uno_contenido_en_otro(largo)
+assert respuesta_corta_a==2, \
+    f"Resultado no condice con el ejemplo, obtuve {str(respuesta_corta_a)}"
+print("	La respuesta A es " + str(respuesta_larga_a))
 
-print("	La respuesta del Ejemplo A es " + str(RespuestaCortaA))
-print("	La respuesta A es " + str(RespuestaA))
-print("	La respuesta del Ejemplo B es " + str(RespuestaCortaB))
-print("	La respuesta B es " + str(RespuestaB))
+respuesta_corta_b = CampCleanup.hay_intersecciones(corto)
+respuesta_larga_b = CampCleanup.hay_intersecciones(largo)
+assert respuesta_corta_b==4, \
+    f"Resultado no condice con el ejemplo, obtuve {str(respuesta_corta_b)}"
+print("	La respuesta B es " + str(respuesta_larga_b))
