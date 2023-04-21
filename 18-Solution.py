e50003ea8f5c433444a1637d21a0863310f44f3d
Plober <plober@gmail.com>
2023-04-21 15:32:23 -0300

    Day 18 answer b

--- 18-Solution.py
+++ 18-Solution.py
@@ -33,25 +33,24 @@ class BoilingBoulders:
             x, y, z = gota.split(",")
             gotas_de_lava.add((int(x), int(y), int(z)))
         self.gotas_de_lava = gotas_de_lava
+        self.alrededor = [(1, 0, 0),
+                          (-1, 0, 0),
+                          (0, 1, 0),
+                          (0, -1, 0),
+                          (0, 0, 1),
+                          (0, 0, -1),
+                          ]
 
-    def superficie_expuesta(self):
+    def superficie_expuesta(self, volumen: set[tuple[int, int, int]]) -> int:
         conteo_caras = 0
-        alrededor = [(1, 0, 0),
-                     (-1, 0, 0),
-                     (0, 1, 0),
-                     (0, -1, 0),
-                     (0, 0, 1),
-                     (0, 0, -1),
-                     ]
-        caras_alrededor = set()
-        caras_alrededor: set[tuple[int, int, int]]
-        for gota in self.gotas_de_lava:
+
+        for gota in volumen:
             conteo_caras += 6
-            for cara in alrededor:
+            for cara in self.alrededor:
                 if (gota[0] + cara[0],
                     gota[1] + cara[1],
-                    gota[2] + cara[2]) in self.gotas_de_lava:
-                    conteo_caras -=1
+                        gota[2] + cara[2]) in volumen:
+                    conteo_caras -= 1
         return conteo_caras
 
     def mostrar_como_puntos(self, input=[]):
@@ -77,53 +76,64 @@ class BoilingBoulders:
 
         return matplotlib.pyplot.show()
 
-    def mostrar_como_cubos(self):
-        import matplotlib.pyplot as plt
-        import numpy as np
+    def espacio_negativo(self, investigar : set[tuple[int, int, int]]) -> set[tuple[int, int, int]]:
+        """Recibe un set de coordenadas y devuelve la figura que esté dentro sin tomar en cuenta huecos."""
+        maximo = max([max(x,y,z) for x,y,z in investigar])
+        iterador = [i for i in range(maximo + 1)]
+        espacio_total = set()
+        espacio_total: set[tuple[int, int, int]]
 
-        plt.style.use('_mpl-gallery')
+        for xs in iterador:
+            for ys in iterador:
+                for zs in iterador:
+                    espacio_total.add((xs, ys, zs))
 
-        # Prepare some coordinates
-        x, y, z = np.indices((8, 8, 8))
+        encavernado = espacio_total - self.gotas_de_lava
 
-        # Draw cuboids in the top left and bottom right corners
-        cube1 = (x < 3) & (y < 3) & (z < 3)
-        cube2 = (x >= 5) & (y >= 5) & (z >= 5)
+        puntos_conectados = {(1, 1, 1)}
+        recien_incorporados = {(1, 1, 1)}
+        cuenta = 0
 
-        # Combine the objects into a single boolean array
-        voxelarray = cube1 | cube2
+        while len(recien_incorporados) != 0 and cuenta < 400:
+            cuenta += 1
+            # print(f"Tengo {len(recien_incorporados)} recién incorporados")
+            capaz_interesantes = set()
 
-        # Plot
-        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-        ax.voxels(voxelarray, edgecolor='k')
+            for lugar in recien_incorporados:
+                for explorar in self.alrededor:
+                    capaz_interesantes.add(
+                        (explorar[0] + lugar[0], explorar[1] + lugar[1], explorar[2] + lugar[2]))
+                    # Carga todos los puntos alrededor de recien_incorporados a capaz_interesantes
+            a_incorporar = capaz_interesantes & encavernado - puntos_conectados
+            # Los puntos a incorporar son los que estaban alrededor de los recién incorporados, pero
+            #     solo si están dentro del cubo a evaluar, y quitándole los que ya sé que están dentro
+            # print(f"Tengo {len(a_incorporar)} a incorporar")
 
-        ax.set(xticklabels=[],
-               yticklabels=[],
-               zticklabels=[])
+            # agrega los recién encontrados a los puntos ya conocidos
+            puntos_conectados |= a_incorporar
+            recien_incorporados = a_incorporar
 
-        plt.show()
+        return espacio_total - puntos_conectados
+    
+    def respuesta_b (self):
+        return self.superficie_expuesta(self.espacio_negativo(self.gotas_de_lava))
+        pass
 
-
-version_corta = BoilingBoulders(corto)
-assert BoilingBoulders(corto).superficie_expuesta() == 64
-# print(
-#     f"Gotas de Lava ({len(version_corta.gotas_de_lava)})= {version_corta.gotas_de_lava}")
-# print(
-#     f"Superficie Expuesta = {version_corta.superficie_expuesta()}")
-
-# print(f"corto_uno {len(BoilingBoulders(corto_uno).superficie_expuesta())}")
-# print(f"corto_dos {len(BoilingBoulders(corto_dos).superficie_expuesta())}")
-# print(f"corto_tres {len(BoilingBoulders(corto_tres).superficie_expuesta())}")
-
-# print(f"Gotas de Lava corto_tres({          len(BoilingBoulders(corto_tres).gotas_de_lava)})= {BoilingBoulders(corto_tres).gotas_de_lava}")
-# print(f"Superficie Expuesta corto_tres({    len(BoilingBoulders(corto_tres).superficie_expuesta())}) = {BoilingBoulders(corto_tres).superficie_expuesta()}")
-
-
-# version_corta.mostrar_como_puntos(BoilingBoulders(corto_tres).gotas_de_lava)
-# version_corta.mostrar_como_puntos(BoilingBoulders(corto_tres).superficie_expuesta())
+RespuestaCorta = BoilingBoulders(corto)
+assert RespuestaCorta.superficie_expuesta(RespuestaCorta.gotas_de_lava) == 64
+assert RespuestaCorta.respuesta_b() == 58
 
 
-RespuestaA, RespuestaB = (BoilingBoulders(largo).superficie_expuesta()), 'N/A'
+
+print("/"*10)
+RespuestaLarga = BoilingBoulders(largo)
+
+mostrar_grafico = False
+if mostrar_grafico:
+    RespuestaCorta.mostrar_como_puntos(RespuestaCorta.espacio_negativo(RespuestaCorta.gotas_de_lava))
+    RespuestaLarga.mostrar_como_puntos()
+
+RespuestaA, RespuestaB = (RespuestaLarga.superficie_expuesta(RespuestaLarga.gotas_de_lava)), RespuestaLarga.respuesta_b()
 
 print("	La respuesta A es " + str(RespuestaA))
 print("	La respuesta B es " + str(RespuestaB))
